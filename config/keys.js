aa931a94446120ceaccd23a9265a4f5f95e58823
Nabin <nabindenepali@outlook.com>
2020-05-07 09:13:57 +0545

    Add login endpoint, returning JWT token

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Worqflow:LantphJGeMRojwAH@cluster0-zjzbp.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://Worqflow:LantphJGeMRojwAH@cluster0-zjzbp.mongodb.net/test?retryWrites=true&w=majority',
+    secretOrKey: 'eIgN51q1KH'
 };
 
 
